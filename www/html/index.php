697c22747839521b487a89f44810b5af9999db93
hivesbee <hivesbee@gmail.com>
2020-06-17 23:22:07 +0900

    initial commit

---
+++ www/html/index.php
@@ -0,0 +1,11 @@
+<?php
+// echo __DIR__
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+$mpdf = new \Mpdf\Mpdf();
+$mpdf->WriteHTML('<h1>Hello world!</h1>');
+$mpdf->Output();
+
+
+// phpinfo();
